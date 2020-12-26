6ff4aaf5f1d25a769e2ef28664171c6ae1efe3b8
MrAinley <ainley.stephan@battlegroundps.org>
2020-12-25 22:09:08 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let prevMillis = control.millis()
+let buttonGate = 100
+let minTime = 50
+let pinVel = 0;
+midi.useRawSerial()
+
+basic.forever(function () {
+    led.unplot(0, 0)
+    // basic.showNumber(control.millis() - prevMillis)
+    pinVel = pins.analogReadPin(AnalogPin.P0)
+    if (pinVel > buttonGate && control.millis() - prevMillis >= minTime) {
+        myThis.setVelocity(map(pinVel, 0, 1024, 1, 127))
+        midi.playDrum(DrumSound.AcousticBassDrum)
+        led.plot(0, 0)
+    }
+    prevMillis = control.millis()
+})
